655a6b2b5cf1a684bca0e23410a8c9b91ec33a7b
woodj22 <joe.wood@bbc.co.uk>
2017-08-13 22:22:58 +0100

    Extract out array map methods for readability

--- APIDoccer/Swagger.py
+++ APIDoccer/Swagger.py
@@ -1,13 +1,19 @@
-
+from string import Template
 
 class Swagger:
-    def __init__(self, path):
+    def __init__(self, path, model_name):
         self.transformerFilePath = path
+        self.model_name = model_name
+        self.property_string = " *     @SWG\Property(property = 'distinguishedName', type = 'string', default = 'cn=blablabl,blalaldosooooo looong name'),"
 
+    def map_model_transformer(self):
         with open(self.transformerFilePath) as f:
             lines = f.readlines()
+
         content = [x.strip() for x in lines]
         found_start = False
+        start_point = 0
+        end_point   = 0
 
         for index, line in enumerate(content):
             if "public $map" in line:
@@ -16,8 +22,19 @@ class Swagger:
             if found_start and "];" in line:
                 end_point = index
 
-        i = self.get_input_names(content[start_point:end_point])
-        print(i[0])
+        return self.get_input_names(content[start_point:end_point])
 
-    def get_input_names(self, mapped_array):
-        return [name.split("=>") for name in mapped_array]
+    def create_swagger_definition(self):
+        filein = open('APIDoccer/swagger_definition.txt')
+        src = Template(filein.read())
+        properties = self.map_model_transformer()
+        details = {'modelName': self.model_name, 'modelPluralName': 'people', 'properties': properties}
+        exit(details)
+        src.substitute(details)
+
+    def get_input_names(self, transformer_array):
+        return list(map(self.make_property, transformer_array))
+
+    @staticmethod
+    def make_property(name):
+        " *     @SWG\Property(property = " + name.split("=>")[0]+ ", type = 'string', default = ''),"
--- __main__.py
+++ __main__.py
@@ -2,4 +2,6 @@ from APIDoccer.Swagger import Swagger
 
 if __name__ == "__main__":
     path = "/Users/JoeWood/dev/api-laravel-mobileapi/app/Transformers/newPersonTransformer.php"
-    swagger = Swagger(path)
+
+    model_name = "Person"
+    swagger = Swagger(path, model_name).create_swagger_definition()
