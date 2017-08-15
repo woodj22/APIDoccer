ad543185a446d4b17b94386f0052c2d1426271f4
woodj22 <joe.wood@bbc.co.uk>
2017-08-15 21:28:41 +0100

    Refactor out get content into its own transformer

--- APIDoccer/Swagger.py
+++ APIDoccer/Swagger.py
@@ -6,24 +6,27 @@ class Swagger:
     def __init__(self, path, model_name):
         self.transformerFilePath = path
         self.model_name = model_name
-        self.property_string = " *     @SWG\Property(property = 'distinguishedName', type = 'string', default = 'cn=blablabl,blalaldosooooo looong name'),"
+        self.content = self.get_content_from_transformer()
 
-    def map_model_transformer(self):
+
+    def get_content_from_transformer(self):
         with open(self.transformerFilePath) as f:
             lines = f.readlines()
 
-        content = [x.strip() for x in lines]
+        return [x.strip() for x in lines]
+
+    def map_model_attributes(self):
 
         array_name = "public $map"
-        i = self.get_array_index_points(content, array_name)
+        i = self.get_array_index_points(self.content, array_name)
+        return self.map_to_property(self.content[i[0]:i[1]])
 
-        return self.get_input_names(content[i[0], i[1]])
 
     def get_array_index_points(self, transformer, array_name):
         found_start = False
         start_point = 0
         end_point = 0
-        
+
         for index, line in enumerate(transformer):
             if array_name in line:
                 start_point = index + 1
@@ -36,7 +39,7 @@ class Swagger:
     def create_swagger_definition(self):
         swagger_template = resource_string(__name__, 'data/swagger_definition.txt')
         src = Template(swagger_template)
-        properties = self.map_model_transformer()
+        properties = self.map_model_attributes()
         details = {
             'modelName': self.model_name,
             'modelPluralName': 'people',
@@ -45,9 +48,9 @@ class Swagger:
 
         return src.substitute(details)
 
-    def get_input_names(self, transformer_array):
+    def map_to_property(self, transformer_array):
         return [self.make_property(name) for name in transformer_array]
 
     @staticmethod
     def make_property(name):
-       return " *     @SWG\Property(property = " + name.split("=>")[0]+ ", type = 'string', default = ''), \n"
+        return " *     @SWG\Property(property = " + name.split("=>")[0]+ ",          type = 'string', default = ''), \n"
