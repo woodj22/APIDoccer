d90b81045791bfe5c48f0bce8fd1abaccadd4ca6
woodj22 <joe.wood@bbc.co.uk>
2017-08-26 15:17:32 +0100

    Add swagger index template and refactor out templating into its own method

--- __main__.py
+++ __main__.py
@@ -3,6 +3,10 @@ from apidoccer.swagger import Swagger
 if __name__ == "__main__":
     path = "/Users/JoeWood/dev/api-laravel-mobileapi/app/Transformers/newPersonTransformer.php"
     #path = "/Users/BBCWood/dev/api-laravel-mobileapi/app/Transformers/PersonTransFormer.php"
-    model_name = "Person"
-    swagger = Swagger(path, model_name).create_swagger_definition()
+
+    attributes = {
+        'model_name': "Person",
+        'plural_model_name': "People"
+                  }
+    swagger = Swagger(path, **attributes).create_swagger_definition()
     print(swagger)
--- apidoccer/swagger.py
+++ apidoccer/swagger.py
@@ -4,17 +4,22 @@ from .php_parser import PHPTransformerParser
 
 
 class Swagger:
-    def __init__(self, path, model_name):
-        self.model_name = model_name
-        self.parser = PHPTransformerParser()
+    def __init__(self, transformer_path, **definition):
 
-        self.content = self.get_content_from_transformer(path)
+        self.model_name = definition.get('model_name')
+        self.plural_model_name = definition.get('plural_model_name')
+        self.parser = PHPTransformerParser()
+        self.content = self.get_content_from_transformer(transformer_path)
 
     @classmethod
     def get_content_from_transformer(cls, path):
         with open(path) as f:
             lines = f.readlines()
         return [x.strip().strip(',') for x in lines]
+
+    # def set_swagger_details(self, detail_input):
+    #    return self.details = detail_input
+    #
 
     def map_casts_to_model_attributes(self):
 
@@ -23,21 +28,26 @@ class Swagger:
 
         return self.parser.map_casts_to_values(map_array, casts_array)
 
+    @staticmethod
+    def add_values_to_template(resource_name, substitute_values):
+        resource = resource_string(__name__, resource_name)
+        swagger_template = Template(resource)
+
+        return swagger_template.substitute(substitute_values)
+
     def create_swagger_definition(self):
 
-        resource = resource_string(__name__, 'data/swagger_definition.txt')
-        swagger_template = Template(resource)
         casted_attributes = self.map_casts_to_model_attributes()
         swagger_properties = self.create_swagger_properties(casted_attributes)
         includes = self.create_available_includes()
         details = {
             'modelName': self.model_name,
-            'modelPluralName': 'people',
+            'modelPluralName': self.plural_model_name,
             'properties': ''.join(swagger_properties),
             'availableIncludes': ''.join(includes)
         }
 
-        return swagger_template.substitute(details)
+        return self.add_values_to_template('data/swagger_definition.txt', details)
 
     def create_available_includes(self):
         available_includes = self.parser.get_transformer_array(self.content, "protected $availableIncludes", False)
@@ -49,15 +59,9 @@ class Swagger:
         return [self.make_swagger_property(attribute, cast) for [attribute, cast] in casted_attributes]
 
     @staticmethod
-    def split_transform_array(array, delimiter=" => ", keep_values=True):
-        if keep_values:
-            return dict(key.split(delimiter) for key in array)
-        else:
-            return [key.split(delimiter)[0] for key in array]
-
-    @staticmethod
     def make_swagger_property(attribute, cast):
         return " *     @SWG\Property(property = " + attribute + ",          type =" + cast + ", default = ''), \n"
 
-    def make_swagger_include_property(self, reference_name):
+    @staticmethod
+    def make_swagger_include_property(reference_name):
         return " *     @SWG\Property(property = " + reference_name + ", type = 'array',@SWG\Items(ref='#/definitions/"+reference_name+"'),),\n"
