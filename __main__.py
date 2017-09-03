c87f0879ef9b8610382163cc94b54ab9f6f51907
woodj22 <joe.wood@bbc.co.uk>
2017-09-03 17:18:04 +0100

    add decode to resource finder

--- __main__.py
+++ __main__.py
@@ -1,12 +1,14 @@
 from apidoccer.swagger import Swagger
 
 if __name__ == "__main__":
-    path = "/Users/JoeWood/dev/api-laravel-mobileapi/app/Transformers/newPersonTransformer.php"
+    transformer_path = "/Users/JoeWood/dev/api-laravel-mobileapi/app/Transformers/newPersonTransformer.php"
     #path = "/Users/BBCWood/dev/api-laravel-mobileapi/app/Transformers/PersonTransFormer.php"
 
     attributes = {
         'model_name': "Person",
-        'plural_model_name': "People"
+        'plural_model_name': "People",
+        'path': 'asf'
                   }
-    swagger = Swagger(**attributes).create_swagger_definition(path)
+    swagger = Swagger(**attributes).create_swagger_definition(transformer_path)
+   # swagger = Swagger(**attributes).create_index_function('people')
     print(swagger)
--- apidoccer/swagger.py
+++ apidoccer/swagger.py
@@ -1,5 +1,5 @@
 from string import Template
-from pkg_resources import resource_string
+from pkg_resources import resource_string, resource_filename
 from .php_parser import PHPTransformerParser
 
 
@@ -14,15 +14,22 @@ class Swagger(PHPTransformerParser):
             lines = f.readlines()
         return [x.strip().strip(',') for x in lines]
 
-
     @staticmethod
     def add_values_to_template(resource_name, substitute_values):
         resource = resource_string(__name__, resource_name)
-        swagger_template = Template(resource)
+        swagger_template = Template(resource.decode('utf-8'))
+        return swagger_template.safe_substitute(substitute_values)
 
-        return swagger_template.substitute(substitute_values)
+    def create_index_function(self, path):
+        details = {
+            'path': path,
+            'modelPluralName': self.plural_model_name,
+            'tag': 'People'
+        }
+        return self.add_values_to_template('data/swagger_index_function_block.txt', details)
 
     def create_swagger_definition(self, transformer_path):
+
         transformer_string = self.map_transformer_to_array(transformer_path)
         swagger_properties = self.create_swagger_properties(self.map_casts_to_model_attributes(transformer_string))
         includes = self.create_available_includes(transformer_string)
